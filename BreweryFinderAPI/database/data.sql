8b4445d0bda776b24832cc5baa409d2b8f5c729d
Nicole <111709554+nicole440@users.noreply.github.com>
2023-04-26 13:31:15 -0400

    Add data file

---
+++ BreweryFinderAPI/database/data.sql
@@ -0,0 +1,14 @@
+SELECT * FROM breweries;
+SELECT brewery_id, brewery_name, street_address, city, zip_code, website FROM breweries;
+SELECT brewery_name, street_address, city, zip_code, website FROM breweries WHERE brewery_name = 'Mad Chef Craft Brewery';
+SELECT brewery_name, street_address, city, zip_code, website FROM breweries WHERE city = 'Lancaster';
+SELECT brewery_name, street_address, city, zip_code, website FROM breweries WHERE zip_code = '17602';
+INSERT INTO breweries (brewery_name, street_address, city, zip_code, website) VALUES ('Test', '100 Main St', 'Ephrata', '17522', 'https://www.example.com/');
+
+SELECT * FROM users;
+INSERT INTO users (date_of_birth, username, password_hash, role) VALUES ('1990/01/01', 'admin', 'password', 'ADMIN');
+INSERT INTO users (date_of_birth, username, password_hash, role) VALUES ('1990/01/01', 'nicole', 'password', 'USER');
+SELECT user_id FROM users WHERE username = 'admin';
+
+SELECT * FROM user_favorites;
+INSERT INTO user_favorites (user_id, brewery_id) VALUES (2, 2);
