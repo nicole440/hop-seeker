04ace2c9c899f54bbaef83fd512dbf4f7c6cc6bc
Nicole <111709554+nicole440@users.noreply.github.com>
2023-05-11 14:33:57 -0400

    Add Brewery Service class for REST API calls

--- BreweryFinderAPI/src/main/java/service/BreweryService.java
+++ BreweryFinderAPI/src/main/java/service/BreweryService.java
@@ -1,2 +1,25 @@
-package service;public class BreweryService {
+package service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class BreweryService {
+
+
+    @GetMapping("/breweries/all")
+    public ResponseEntity<String> getAllBreweries() {
+        String apiUrl = "https://api.openbrewerydb.org/v1/breweries";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer <your-access-token>");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
+
+        return response;
+    }
 }
