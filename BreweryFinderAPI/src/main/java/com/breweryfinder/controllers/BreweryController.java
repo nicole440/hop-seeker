3fffee3a5e1601b3b790435c8c202a8778307fb9
Nicole <111709554+nicole440@users.noreply.github.com>
2023-05-11 08:58:44 -0400

    Revert changes

--- BreweryFinderAPI/src/main/java/com/breweryfinder/controllers/BreweryController.java
+++ BreweryFinderAPI/src/main/java/com/breweryfinder/controllers/BreweryController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("https://api.openbrewerydb.org/v1/breweries")
 @CrossOrigin // Allow calls within domain but not from same port
 public class BreweryController {
 
@@ -18,22 +17,22 @@ public class BreweryController {
         this.breweryDao = breweryDao;
     }
 
-    @RequestMapping(path="/", method = RequestMethod.GET)
+    @RequestMapping(path="/breweries/all", method = RequestMethod.GET)
     public List<Brewery> getAllBreweries() {
         return breweryDao.getAllBreweries();
     }
 
-    @RequestMapping(path="?by_name={breweryName}&per_page=3", method = RequestMethod.GET)
+    @RequestMapping(path="/breweries/name/{breweryName}", method = RequestMethod.GET)
     public List<Brewery> searchBreweriesByName(@PathVariable String breweryName) {
         return breweryDao.searchBreweriesByName(breweryName);
     }
 
-    @RequestMapping(path="?by_city={city}&per_page=3", method = RequestMethod.GET)
+    @RequestMapping(path="/breweries/city/{city}", method = RequestMethod.GET)
     public List<Brewery> getBreweriesByCity(@PathVariable String city) {
         return breweryDao.getBreweriesByCity(city);
     }
 
-    @RequestMapping(path="?by_postal={zipCode}&per_page=3", method = RequestMethod.GET)
+    @RequestMapping(path="/breweries/zip/{zipCode}", method = RequestMethod.GET)
     public List<Brewery> getBreweriesByZip(@PathVariable String zipCode) {
         return breweryDao.getBreweriesByZip(zipCode);
     }
