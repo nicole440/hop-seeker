29ae39276134db26dd5588719943c2345eb69111
Nicole <111709554+nicole440@users.noreply.github.com>
2023-05-17 17:38:48 -0400

    Add intro paragraph and button for location-based search

--- brewery-finder-ui/src/App.js
+++ brewery-finder-ui/src/App.js
@@ -9,14 +9,19 @@ function App() {
         <h1>HopSeeker</h1>
         <h3>Wander. Sip. Repeat.</h3>
       </header>
+      <div class="about">
+        <p class="main">Discover Pennsylvania's Finest Breweries with HopSeeker! Wander through the world of craft beer, sip on exquisite flavors, and repeat the adventure.</p>
+        <p class="coming-soon">Coming soon: Find breweries near you!</p>
+      </div>
       <div class="search-container">
         <form class="search-form">
           <input class="search" type="text" name="name" placeholder="Name"></input>
           <input class="search" type="text" name="city" placeholder="City"></input>
-          <input class="search" type="text" name="state" placeholder="State"></input>
+          {/* <input class="search" type="text" name="state" placeholder="State"></input> */}
           <input class="search" type="text" name="zip" placeholder="Zip Code"></input>
         </form>
         <button type="submit" class="search">Search</button>
+        <button type="button" class="location">Beer Near Here</button>
       </div>
     </div>
   );
