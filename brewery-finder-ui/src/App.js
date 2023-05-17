e375b1c1da3ee990f4c8707615fc5dbba4c79e02
Nicole <111709554+nicole440@users.noreply.github.com>
2023-05-17 18:25:06 -0400

    Minor style tweaking

--- brewery-finder-ui/src/App.js
+++ brewery-finder-ui/src/App.js
@@ -10,14 +10,13 @@ function App() {
         <h3>Wander. Sip. Repeat.</h3>
       </header>
       <div class="about">
-        <p class="main">Discover Pennsylvania's Finest Breweries with HopSeeker! Wander through the world of craft beer, sip on exquisite flavors, and repeat the adventure.</p>
+        <p class="main">Explore Pennsylvania's Finest Craft Breweries</p>
         <p class="coming-soon">Coming soon: Find breweries near you!</p>
       </div>
       <div class="search-container">
         <form class="search-form">
-          <input class="search" type="text" name="name" placeholder="Name"></input>
+          <input class="search" type="text" name="name" placeholder="Brewery Name"></input>
           <input class="search" type="text" name="city" placeholder="City"></input>
-          {/* <input class="search" type="text" name="state" placeholder="State"></input> */}
           <input class="search" type="text" name="zip" placeholder="Zip Code"></input>
         </form>
         <button type="submit" class="search">Search</button>
