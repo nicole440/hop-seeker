b76f577bdb888662851290e12f279fef0c7ad4e3
Nicole <111709554+nicole440@users.noreply.github.com>
2023-06-30 10:10:44 -0400

    Update DAO methods based on additional Brewery attributes.

--- BreweryFinderAPI/src/main/java/com/breweryfinder/dao/JdbcBreweryDao.java
+++ BreweryFinderAPI/src/main/java/com/breweryfinder/dao/JdbcBreweryDao.java
@@ -20,7 +20,7 @@ public class JdbcBreweryDao implements BreweryDao {
     // SQL query tested in pgAdmin - Success
     @Override
     public List<Brewery> getBreweries() {
-        String sql = "SELECT brewery_id, brewery_name, street_address, city, zip_code, website " +
+        String sql = "SELECT brewery_id, brewery_name, street_address, city, zip_code, latitude, longitude, website " +
                 "FROM breweries;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
         List<Brewery> breweryList = new ArrayList<>();
@@ -33,7 +33,7 @@ public class JdbcBreweryDao implements BreweryDao {
     // SQL query tested in pgAdmin - Success
     @Override
     public List<Brewery> getBreweriesByName(String breweryName) {
-        String sql = "SELECT brewery_id, brewery_name, street_address, city, zip_code, website " +
+        String sql = "SELECT brewery_id, brewery_name, street_address, city, zip_code, latitude, longitude, website " +
                 "FROM breweries " +
                 "WHERE brewery_name ILIKE ?;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, breweryName);
@@ -47,7 +47,7 @@ public class JdbcBreweryDao implements BreweryDao {
     // SQL query tested in pgAdmin - Success
     @Override
     public List<Brewery> getBreweriesByCity(String city) {
-        String sql = "SELECT brewery_id, brewery_name, street_address, city, zip_code, website " +
+        String sql = "SELECT brewery_id, brewery_name, street_address, city, zip_code, latitude, longitude, website " +
                 "FROM breweries " +
                 "WHERE city = ?;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, city);
@@ -61,7 +61,7 @@ public class JdbcBreweryDao implements BreweryDao {
     // SQL query tested in pgAdmin - Success
     @Override
     public List<Brewery> getBreweriesByZip(String zipCode) {
-        String sql = "SELECT brewery_id, brewery_name, street_address, city, zip_code, website " +
+        String sql = "SELECT brewery_id, brewery_name, street_address, city, zip_code, latitude, longitude, website " +
                 "FROM breweries " +
                 "WHERE zip_code = ?;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, zipCode);
@@ -75,7 +75,7 @@ public class JdbcBreweryDao implements BreweryDao {
     // SQL query tested in pgAdmin - Success
     @Override
     public List<Brewery> getFavoritesByUserId(int userId) {
-        String sql = "SELECT b.brewery_id, brewery_name, street_address, city, zip_code, website " +
+        String sql = "SELECT b.brewery_id, brewery_name, street_address, city, zip_code, latitude, longitude, website " +
                 "FROM breweries AS b " +
                 "JOIN user_favorites AS uf ON uf.brewery_id = b.brewery_id " +
                 "WHERE uf.user_id = ?;";
@@ -90,19 +90,21 @@ public class JdbcBreweryDao implements BreweryDao {
     // SQL query tested in pgAdmin - Success
     @Override
     public boolean addNewBrewery(Brewery newBrewery) {
-        String sql = "INSERT INTO breweries (brewery_name, street_address, city, zip_code, website) " +
-                "VALUES (?, ?, ?, ?, ?);";
-        jdbcTemplate.update(sql, newBrewery.getBreweryName(), newBrewery.getBreweryStreetAddress(), newBrewery.getBreweryCity(), newBrewery.getBreweryZipCode(), newBrewery.getBreweryWebsite());
+        String sql = "INSERT INTO breweries (brewery_id, brewery_name, street_address, city, zip_code, latitude, longitude, website) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        jdbcTemplate.update(sql, newBrewery.getBreweryId(), newBrewery.getBreweryName(), newBrewery.getBreweryStreetAddress(), newBrewery.getBreweryCity(), newBrewery.getBreweryZipCode(), newBrewery.getLatitude(), newBrewery.getLongitude(), newBrewery.getBreweryWebsite());
         return newBrewery == null ? false : true;
     }
 
     private Brewery mapRowToBreweries(SqlRowSet rowSet) {
         Brewery brewery = new Brewery();
-        brewery.setBreweryId(rowSet.getInt("brewery_id"));
+        brewery.setBreweryId(rowSet.getString("brewery_id"));
         brewery.setBreweryName(rowSet.getString("brewery_name"));
         brewery.setBreweryStreetAddress(rowSet.getString("street_address"));
         brewery.setBreweryCity(rowSet.getString("city"));
         brewery.setBreweryZipCode(rowSet.getString("zip_code"));
+        brewery.setLatitude(rowSet.getString("latitude"));
+        brewery.setLongitude(rowSet.getString("longitude"));
         brewery.setBreweryWebsite(rowSet.getString("website"));
         return brewery;
     }
