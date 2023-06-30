bc5977919e18023652e5490141951a7ae55afcde
Nicole <111709554+nicole440@users.noreply.github.com>
2023-06-30 10:06:53 -0400

    Update schema

--- Database/BreweryDB.sql
+++ Database/BreweryDB.sql
@@ -1,7 +1,9 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users, breweries CASCADE;
-DROP SEQUENCE IF EXISTS user_serial, brewery_serial;
+DROP TABLE IF EXISTS users, breweries, user_favorites CASCADE;
+DROP SEQUENCE IF EXISTS user_serial;
+
+--CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; for eventually utilizing UUID for unique ID numbers
 
 CREATE SEQUENCE user_serial;
 CREATE TABLE users (
@@ -13,28 +15,32 @@ CREATE TABLE users (
 	CONSTRAINT PK_user_id PRIMARY KEY(user_id)
 );
 
-CREATE SEQUENCE brewery_serial;
 CREATE TABLE breweries (
-    brewery_id INT NOT NULL DEFAULT nextval('brewery_serial'),
+    brewery_id VARCHAR(100) NOT NULL,
     brewery_name VARCHAR(50) NOT NULL,
     street_address VARCHAR(50) NULL,
     city VARCHAR(20) NOT NULL,
-    zip_code VARCHAR(5) NOT NULL,
+    zip_code VARCHAR(10) NOT NULL,
+	longitude VARCHAR(15) NULL,
+    latitude VARCHAR(15) NULL,
     website VARCHAR(200) NULL,
 	CONSTRAINT PK_brewery_id PRIMARY KEY(brewery_id)
 );
 
+
 CREATE TABLE user_favorites (
 	user_id INT,
-	brewery_id INT,
+	brewery_id VARCHAR(100),
 	CONSTRAINT FK_user_id FOREIGN KEY(user_id) REFERENCES users(user_id),
 	CONSTRAINT FK_brewery_id FOREIGN KEY(brewery_id) REFERENCES breweries(brewery_id)
 );
 
-INSERT INTO breweries (brewery_name, street_address, city, zip_code, website) VALUES ('Mad Chef Craft Brewery', '2023 Miller Rd', 'East Petersburg', '17520', 'https://www.madchefcraftbrewing.com/');
-INSERT INTO breweries (brewery_name, street_address, city, zip_code, website) VALUES ('Our Town Brewery', '252 N Prince St', 'Lancaster', '17603', 'https://ourtownbrewery.com/');
-INSERT INTO breweries (brewery_name, street_address, city, zip_code, website) VALUES ('Lancaster Brewing Company', '302 N Plum St', 'Lancaster', '17602', 'https://www.lancasterbrewing.com/');
-
 COMMIT;
 
 ROLLBACK;
+
+--INSERT INTO breweries (brewery_name, street_address, city, zip_code, website) VALUES ('Mad Chef Craft Brewery', '2023 Miller Rd', 'East Petersburg', '17520', 'https://www.madchefcraftbrewing.com/');
+--INSERT INTO breweries (brewery_name, street_address, city, zip_code, website) VALUES ('Our Town Brewery', '252 N Prince St', 'Lancaster', '17603', 'https://ourtownbrewery.com/');
+--INSERT INTO breweries (brewery_name, street_address, city, zip_code, website) VALUES ('Lancaster Brewing Company', '302 N Plum St', 'Lancaster', '17602', 'https://www.lancasterbrewing.com/');
+
+
